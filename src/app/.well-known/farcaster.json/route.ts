32fa28cbb16e57c00ec4b72d602c26688fd80a1a
Ryan <anpx@moonlab.asia>
2025-10-28 16:24:38 +0700

    Update account association fields in farcaster.json configuration

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,9 +3,11 @@ export async function GET() {
 
   const config = {
     accountAssociation: {
-      header: "",
-      payload: "",
-      signature: "",
+      header:
+        "eyJmaWQiOjE0MTk2MzQsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyMTE3NGFkMDg0ODg4ODAzNjk0ZjEyZTQzZTQ3NkY0ZENmYURCNjg5In0",
+      payload: "eyJkb21haW4iOiJmYXJjYXN0ZXItZGVtby10ZW4udmVyY2VsLmFwcCJ9",
+      signature:
+        "4z1cZrcYYH+G3a4tLFle23cDeEMlIX5/5+zvtjeZZDxgXse4A74+B0wRd2Y6mrHAsoaEQ+89CRfmW9e+jWPpbhs=",
     },
     frame: {
       name: "Interview app",
@@ -13,8 +15,7 @@ export async function GET() {
       iconUrl: "https://farcaster-demo-ten.vercel.app/icon.png",
       homeUrl: "https://farcaster-demo-ten.vercel.app",
       imageUrl: "https://farcaster-demo-ten.vercel.app/image.png",
-      splashImageUrl:
-        "https://farcaster-demo-ten.vercel.app/splash.png",
+      splashImageUrl: "https://farcaster-demo-ten.vercel.app/splash.png",
       splashBackgroundColor: "#ffffff",
       webhookUrl: "https://farcaster-demo-ten.vercel.app/api/webhook",
       subtitle: "connect and interview",
